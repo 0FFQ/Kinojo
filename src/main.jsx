a8372b793acb998bbe3df05ef66629541f7f5f53
0FFQ <denisov468968@mail.ru>
2025-11-04 15:49:40 +0300

    исправление по деплою в main

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,15 @@ import { store } from "./app/store";
 import darkTheme from "./app/theme";
 import App from "./components/App";
 import "bear-react-carousel/dist/index.css";
+import { BrowserRouter } from "react-router-dom"; // <-- добавили
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <App />
+      <BrowserRouter basename="/Kinojo"> {/* <-- добавили */}
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </Provider>,
 );
